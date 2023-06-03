6fb265bfdb8649d42a938314a3ebde892befb248
matias tello <tellomatias17@gmail.com>
2023-06-03 11:31:07 -0300

    modificando

--- codigo.js
+++ codigo.js
@@ -1,4 +1,21 @@
+const formulario = document.getElementById("formulario")
 
 
+formulario.addEventListener('submit', (e)=>{
+e.preventDefault()
+
+let nombre = document.getElementById("nombre")
+let apellido = document.getElementById("apellido")
+let texto = document.getElementById("texto")
+
+    if(nombre.value.length <3 || apellido.value.length < 3){
+
+ texto.innerHTML="Los campos deben contener al menos 3 caracteres"
 
 
+    } else{
+        texto.innerHTML=""
+    }
+
+
+})
